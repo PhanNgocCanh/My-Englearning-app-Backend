24d49c9b5bd001fa5d08b4ab62235d04c9761880
PhanNgocCanh <canhrapter@gmail.com>
2023-09-26 14:58:55 +0700

    Initial version of ExerciseApp generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/exerciseapp/myapp/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.exerciseapp.myapp.repository;
+
+import com.exerciseapp.myapp.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/exerciseapp/myapp/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.exerciseapp.myapp.service.dto;
